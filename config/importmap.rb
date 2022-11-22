1c17ecfb57d5575c39aa092e9879a8ece99ad5e3
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-11-22 20:57:57 -0300

    Working with selectively importing modules.

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,13 @@
-# Pin npm packages by running ./bin/importmap
+# frozen_string_literal: true
 
+# Pin npm packages by running ./bin/importmap
 pin 'application', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'alpine-turbo-drive', to: 'https://cdn.jsdelivr.net/npm/alpine-turbo-drive-adapter@1.0.x/dist/alpine-turbo-drive-adapter.min.js'
-pin 'alpinejs', to: 'https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js'
-pin 'alpine-ie11', to: 'https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine-ie11.min.js'
+
+# Selectively importing modules
+pin 'alpine-turbo-drive', preload: true
+pin 'alpinejs', preload: true
+pin 'alpine-ie11', preload: true
