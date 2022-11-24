2e39853409289b0c5008c7b70ee3273c97271caf
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-11-23 21:49:58 -0300

    Add model tests.

--- test/models/income_test.rb
+++ test/models/income_test.rb
@@ -1,7 +1,23 @@
-require "test_helper"
+require 'test_helper'
 
 class IncomeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Income attributes must not be empty' do
+    income = Income.new
+    assert income.invalid?
+    assert income.errors[:name].any?
+    assert income.errors[:amount].any?
+    assert income.errors[:payday].any?
+  end
+
+  test 'Income amount must be a positive' do
+    income = Income.new(name: 'John Doe salary', payday: '2022-11-01')
+    income.amount = -1
+    assert income.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], income.errors[:amount]
+    income.amount = 0
+    assert income.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], income.errors[:amount]
+    income.amount = 1
+    assert income.valid?
+  end
 end
