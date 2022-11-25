1aeccc257c94f19242f6aa177755508195b608ef
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-11-24 22:35:19 -0300

    Adds comment

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// The unprocessable_entity breaks the javascript to disable turbo uncomment the line below
+// Turbo.session.drive = false
