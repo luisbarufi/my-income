88b453b268f5ee037d4c935e28dd7bd38e41e3ba
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-11-29 22:38:07 -0300

    Fix the tests

--- test/models/income_test.rb
+++ test/models/income_test.rb
@@ -29,10 +29,10 @@ class IncomeTest < ActiveSupport::TestCase
     income = Income.new(name: 'John Doe salary', payday: '2022-11-01')
     income.amount = -1
     assert income.invalid?
-    assert_equal ['must be greater than or equal to 0.01'], income.errors[:amount]
+    assert_equal [I18n.t('test.errors.messages.must_be_greater_than_or_equal_to', count: '0.01')], income.errors[:amount]
     income.amount = 0
     assert income.invalid?
-    assert_equal ['must be greater than or equal to 0.01'], income.errors[:amount]
+    assert_equal [I18n.t('test.errors.messages.must_be_greater_than_or_equal_to', count: '0.01')], income.errors[:amount]
     income.amount = 1
     assert income.valid?
   end
